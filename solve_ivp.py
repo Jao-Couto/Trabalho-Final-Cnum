cc485b078c5a8016fb2447706493216b27dc2565
Jao-Couto <73503848+Jao-Couto@users.noreply.github.com>
2021-08-08 19:49:13 -0300

    done

--- solve_ivp.py
+++ solve_ivp.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate._ivp.common import norm
 
 
-def func(t, v):
+def funcF(t, v):
     F = lambda x: 0.0039 + 0.0058/(1+np.exp(0.2*(x-35))) 
     normal = np.sqrt(v[0]**2+v[1]**2+v[2]**2)
     B = 4.1 * 10**-4
@@ -15,21 +15,39 @@ def func(t, v):
 
     return np.array([f0, f1, f2])
 
-t_interval = [0, 0.5]                                    # Intervalo de t
-conds = [38*np.cos(1), 0, 38*np.sin(1)]                  # Condição inicial (de u_1 e u_2 e u_3)
-t = np.linspace(t_interval[0], t_interval[1], 51)        # definir as divisões (implica na escolha do h)
-u = solve_ivp(func, t_interval, conds, t_eval=t).y       # A solução fica armazenada no atributo y do objeto de retorno, por isso ".y"
+def funcU(t, u):
+    s1 = u[0]*t
+    s2 = u[1]*t
+    s3 = u[2]*t
+    return np.array([s1,s2,s3])
 
-print(u[2][-10])
-print(t[-10])
-# Cada linha de u armazena a solução da equaçao respectiva
-plt.figure(figsize=(7,4), dpi=100)
-plt.plot(t, u[0], label='$u_1$ ')
-plt.plot(t, u[1], label='$u_2$ ')
-plt.plot(t, u[2], label='$u_3$ ')
-plt.title('2, Solve_ivp, h = %f' % (t[1]-t[0]))
+t_interval = [0, 3]    
+t = np.linspace(t_interval[0], t_interval[1], 300) 
+
+v0 = [38*np.cos(1), 0, 38*np.sin(1)]              
+V = solve_ivp(funcF, t_interval, v0, t_eval=t).y  
+
+
+s1 = []
+s2 = []
+s3 = []
+aux = True
+for i in range(0,300):
+    s1.append((V[0,i])*(t[i]))
+    s2.append((V[1,i])*(t[i]))
+    s3.append((V[2,i])*(t[i]))
+
+    if s3[-1] < 0:
+        s3[-1] = 0
+        if aux: 
+            tempo_solve = t[i]
+            print("\nTempo aproximado para atingir o chão com solve_ivp %f"%t[i])
+        aux = False
+
+plt.figure(4,figsize=(7,4), dpi=100)
+plt.plot(t, s1, label='x')
+plt.plot(t, s2, label='y')
+plt.plot(t, s3, label='z')
+plt.title('2, solve_ivp, h = %1.4f' % (t[1]-t[0]))
 plt.legend()
-plt.show()
 
-
-#Atinge o chão depois de 2.5 segundos
--- trabalho.py
+++ trabalho.py
@@ -25,7 +25,7 @@ def calcErro(uAprox, uExata,t_interv, n):
         nome = "u"+str(i)
         errofun = [nome]
         for j in range(0, len(t)):
-            errofun.append(uExata[i](t[j])-uAprox[i,j])
+            errofun.append(abs(uExata[i](t[j])-uAprox[i,j]))
         erro.append(errofun)
     return erro
 
@@ -67,6 +67,7 @@ N = 6
 ex = [u1,u2]
 header = [str(x) for x in range(1, N+1)]
 erroExp = calcErro(uExp, ex, inter, N)
+print("\nExercício 1a\n")
 print("\nErro Explícito:\n")
 print(tabulate(erroExp,headers=header))
 
@@ -103,16 +104,20 @@ u2 = lambda t: -3*np.exp(-t) + 2*np.exp(-2*t)
 ex = [u1,u2]
 header = [str(x) for x in range(1, N+1)]
 erroExp = calcErro(uExp, ex, inter, N)
+print("\nExercício 1b\n")
 print("\nErro Explícito:\n")
-print(tabulate(erroExp,headers=header))
+erroExp = np.transpose(erroExp)
+print(tabulate(erroExp))
 
 erroMel = calcErro(uMel, ex, inter, N)
+erroMel = np.transpose(erroMel)
 print("\nErro Melhorada:\n")
-print(tabulate(erroMel,headers=header))
+print(tabulate(erroMel))
 
 erroMod = calcErro(uMod, ex, inter, N)
+erroMod = np.transpose(erroMod)
 print("\nErro Modificada:\n")
-print(tabulate(erroMod,headers=header))
+print(tabulate(erroMod))
 
 exata = calcExata(ex,inter,N)
 plot(uExp, uMel, uMod, exata, inter, N, "1b")
@@ -140,6 +145,7 @@ u3 = lambda t: np.cos(t) - np.sin(t)
 ex = [u1,u2, u3]
 header = [str(x) for x in range(1, N+1)]
 erroExp = calcErro(uExp, ex, inter, N)
+print("\nExercício 1c\n")
 print("\nErro Explícito:\n")
 print(tabulate(erroExp,headers=header))
 
@@ -178,6 +184,7 @@ u3 = lambda t: 0.25*t**4 + t - np.exp(-t)
 ex = [u1,u2, u3]
 header = [str(x) for x in range(1, N)]
 erroExp = calcErro(uExp, ex, inter, N)
+print("\nExercício 1d\n")
 print("\nErro Explícito:\n")
 print(tabulate(erroExp,headers=header))
 
@@ -207,17 +214,93 @@ def func2(t, v):
 
     return np.array([f0, f1, f2])
 
-inter = [0,0.5] 
+inter = [0,3] 
 un = [38*np.cos(1), 0, 38*np.sin(1)] 
-N = 51
+N = 300
 
 uExp = eulerExplicito(func2, inter, un, N)
 uMel = eulerMelhorado(func2, inter, un, N)
 uMod = eulerModificado(func2, inter, un, N)
+   
+t = np.linspace(inter[0], inter[1], 300) 
+s1uExp = []
+s2uExp = []
+s3uExp = []
 
-exata = calcExata(ex,inter,N)
-plot(uExp, uMel, uMod, exata, inter, N, "2")
+s1uMel = []
+s2uMel = []
+s3uMel = []
+
+s1uMod = []
+s2uMod = []
+s3uMod = []
+
+aux1 = True
+aux2 = True
+aux3 = True
+
+from solve_ivp import tempo_solve
+import solve_ivp
+
+for i in range(0,300):
+    s1uExp.append((uExp[0,i])*(t[i]))
+    s2uExp.append((uExp[1,i])*(t[i]))
+    s3uExp.append((uExp[2,i])*(t[i]))
+
+    s1uMel.append((uMel[0,i])*(t[i]))
+    s2uMel.append((uMel[1,i])*(t[i]))
+    s3uMel.append((uMel[2,i])*(t[i]))
+
+    s1uMod.append((uMod[0,i])*(t[i]))
+    s2uMod.append((uMod[1,i])*(t[i]))
+    s3uMod.append((uMod[2,i])*(t[i]))
+    
+    if s3uExp[-1] < 0:
+        s3uExp[-1] = 0
+        if aux1: 
+            erroExp = abs(t[i] - tempo_solve)
+            print("Tempo aproximado para atingir o chão no Euler Explícito %f, com erro de %f"%(t[i],erroExp))
+        aux1 = False
+
+    if s3uMel[-1] < 0:
+        s3uMel[-1] = 0
+        if aux2:
+            erroMel = abs(t[i] - tempo_solve) 
+            print("Tempo aproximado para atingir o chão no Euler Melhorado %f, com erro de %f"%(t[i],erroMel))
+            
+        aux2 = False
+
+    if s3uMod[-1] < 0:
+        s3uMod[-1] = 0
+        if aux3:
+            erroMod = abs(t[i] - tempo_solve) 
+            print("Tempo aproximado para atingir o chão no Euler Modificado %f, com erro de %f"%(t[i],erroMod))
+            
+        aux3 = False
+
+plt.figure(1)
+plt.plot(t, s1uExp, label='x')
+plt.plot(t, s2uExp, label='y')
+plt.plot(t, s3uExp, label='z')
+plt.title('2, Explícito , h = %1.4f' % (t[1]-t[0]))
+plt.legend()
+
+plt.figure(2)
+plt.plot(t, s1uMod, label='x')
+plt.plot(t, s2uMod, label='y')
+plt.plot(t, s3uMod, label='z')
+plt.title('2, Modificado , h = %1.4f' % (t[1]-t[0]))
+plt.legend()
+
+plt.figure(3)
+plt.plot(t, s1uMel, label='x')
+plt.plot(t, s2uMel, label='y')
+plt.plot(t, s3uMel, label='z')
+plt.title('2, Melhorado , h = %1.4f' % (t[1]-t[0]))
+plt.legend()
 plt.show()
 
-import solve_ivp
+
+
+
 #Atinge o chão depois de 2.5 segundos
